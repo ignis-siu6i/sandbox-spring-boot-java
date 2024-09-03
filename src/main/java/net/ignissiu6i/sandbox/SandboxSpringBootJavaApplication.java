9c9b75e5f6b0bca7dcfce82b40d6402157653522
Seunghwan @vdi0 <sh_63.park@samsung.com>
2024-09-03 16:33:59 +0900

    initial commit

---
+++ src/main/java/net/ignissiu6i/sandbox/SandboxSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package net.ignissiu6i.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxSpringBootJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxSpringBootJavaApplication.class, args);
+    }
+
+}
